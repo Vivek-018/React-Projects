8a159cc30841b051dfc66a2df012d9ada01c18ec
Vivek kumar <111423295+Vivek-018@users.noreply.github.com>
2024-04-12 08:33:43 +0000

    Add functionality X

--- Tic-tac-toe/src/components/Board.jsx
+++ Tic-tac-toe/src/components/Board.jsx
@@ -1,23 +1,33 @@
+import { useState } from "react";
 import Square from "./Square";
 
 function Board() {
+
+    const [squares, setsquares] = useState(Array(9).fill(null));
+
+    function handleClick(i) {
+
+        const nextsquares = squares.slice();
+        nextsquares[i] = "X";
+        setsquares(nextsquares);
+    }
+
     return <>
 
         <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
-
+            <Square value={squares[0]} onSquareclick={() => { handleClick(0) }} />
+            <Square value={squares[1]} onSquareclick={() => { handleClick(1) }} />
+            <Square value={squares[2]} onSquareclick={() => { handleClick(2) }} />
         </div>
         <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
+            <Square value={squares[3]} onSquareclick={() => { handleClick(3) }} />
+            <Square value={squares[4]} onSquareclick={() => { handleClick(4) }} />
+            <Square value={squares[5]} onSquareclick={() => { handleClick(5) }} />
         </div>
         <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
+            <Square value={squares[6]} onSquareclick={() => { handleClick(6) }} />
+            <Square value={squares[7]} onSquareclick={() => { handleClick(7) }} />
+            <Square value={squares[8]} onSquareclick={() => { handleClick(8) }} />
         </div>
 
 
