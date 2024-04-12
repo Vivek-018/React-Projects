12cb66e96cc4c1ee4e6ff4e8949ec95b390366dc
Vivek kumar <111423295+Vivek-018@users.noreply.github.com>
2024-04-12 09:35:37 +0000

    create Header component

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -1,11 +1,11 @@
-
+import Header from "./Components/Header";
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1>E-commerce Project</h1>
+      <Header />
     </>
   )
 }
---
+++ E-commerce/src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+function Header() {
+    return (
+        <nav className="navbar">
+            <div className="container">
+                <ul>
+                    <li>cart</li>
+                    <li>cart</li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Header;
